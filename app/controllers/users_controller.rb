d76bec6a57aa872e32ef1da71042a9f680c45663
m-h-miller <m.harry.miller@gmail.com>
2015-11-17 17:14:55 -0500

    prepare for initial deployment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,8 @@ class UsersController < ApplicationController
   end
 
   private
-
     def user_params
       params.require(:user).permit(:user_name, :session_token, :password)
     end
-
 
 end
