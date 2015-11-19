06234e37db5ec74b200aed75cb6f82e5564a82b3
m-h-miller <m.harry.miller@gmail.com>
2015-11-19 09:47:17 -0500

    Begin questions_form component using LinkedStateMixin

---
+++ app/assets/javascripts/components/questions/questions_form.js.jsx
@@ -0,0 +1,42 @@
+window.QuestionsForm = React.createClass({
+  mixins: [React.addons.LinkedStateMixin, ReactRouter.History],
+
+  defaults: {
+    title: "",
+    body: ""
+  },
+
+  getInitialState: function () {
+    return this.defaults;
+  },
+
+  submitQuestion: function (e) {
+    e.preventDefault();
+    var question = {};
+  },
+
+  render: function () {
+    return(
+      <form className='new-question-form' onSubmit={this.submitQuestion}>
+        <div>
+          <label htmlFor='question_title'>Title:</label>
+          <input
+            type='text'
+            id='question_title'
+            valueLink={this.linkState('title')} />
+        </div>
+
+        <div>
+          <label htmlFor='question_body'>Body:</label>
+          <input
+            type='text'
+            id='question_body'
+            valueLink={this.linkState('body')} />
+        </div>
+
+        <button> Ask Question </button>
+        <br/>
+      </form>
+    );
+  }
+});
