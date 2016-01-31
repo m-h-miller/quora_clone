4ede8477baa194fd106b2b05daed2484940e7161
Harry <m.harry.miller@gmail.com>
2016-01-30 20:03:27 -0500

    Middle of refactor for question store w/ newer syntax

--- frontend/javascripts/stores/questions.js
+++ frontend/javascripts/stores/questions.js
@@ -1,15 +1,17 @@
-var Dispatcher = require('./../dispatcher/dispatcher.js'),
-    QuestionConstants = require('../constants/question_constants.js');
+var AppDispatcher = require('./../dispatcher/dispatcher.js'),
+    QuestionConstants = require('../constants/question_constants.js'),
+    Store = require('flux/utils').Store;
 
-var QUESTIONS_INDEX_CHANGE_EVENT = "questionsIndexChange";
-var ANSWERS_INDEX_CHANGE_EVENT = "answersIndexChange";
+var QUESTIONS_INDEX_CHANGE_EVENT = "questionsIndexChange",
+    ANSWERS_INDEX_CHANGE_EVENT = "answersIndexChange";
 
-var _questions = [];
-var _answers = [];
+var _questions = [],
+    _answers = [];
+
+var QuestionStore = new Store(AppDispatcher);
 
 var resetQuestions = function (questions) {
-  _questions = questions;
-  QuestionStore.changed();
+  _questions = questions.slice(0);
 };
 
 var resetQuestion = function (question) {
@@ -20,83 +22,86 @@ var resetQuestion = function (question) {
       updated = true;
     }
   });
-  if (!updated) { _questions.push(question); }
-  QuestionStore.changed();
+  if (!updated) {
+    _questions.push(question);
+  }
 };
 
 var resetAnswers = function (answers) {
   _answers = answers.reverse();
-  QuestionStore.answersChanged();
 };
 
 var addAnswer = function (answer) {
   _answers.push(answer);
-  QuestionStore.answersChanged();
 };
 
-var QuestionStore = $.extend({}, EventEmitter.prototype, {
+QuestionStore.all = function () {
+  return _question.slice(0);
+};
 
-  all: function () {
-    return _questions.slice(0);
-  },
+QuestionStore.find = function (id) {
+  var question;
+  _questions.forEach(function(q) {
+    if (q.id === id) { question = q; }
+  });
+  return question;
+};
 
-  find: function (id) {
-    var question;
-    _questions.forEach(function(q) {
-      if (q.id === id) { question = q; }
-    });
-    return question;
-  },
+// Double check wtf I'm using this method for
+QuestionStore.findAuthorQuestions = function (user_id) {
+  var questions = [];
+  _questions.forEach(function(q) {
+    if (q.author_id === user_id) { questions.push(q); }
+  });
+  return questions;
+};
 
-  findAuthorQuestions: function (user_id) {
-    var questions = [];
-    _questions.forEach(function(q) {
-      if (q.author_id === user_id) { questions.push(q); }
-    });
-    return questions;
-  },
+// ditto.
+QuestionStore.allQuestionAnswers = function () {
+  return _answer.slice(0);
+};
 
-  allQuestionAnswers: function () {
-    return _answers.slice(0);
-  },
+QuestionStore.__onDispatch = function (payload) {
+  switch(payload.actionType) {
+    case QuestionConstants.QUESTIONS_RECEIVED:
+        resetQuestions(payload.questions);
+        QuestionStore.__emitChange();
+      break;
+    case QuestionConstants.QUESTION_RECEIVED:
+        resetQuestion(payload.question);
+        QuestionStore.__emitChange();
+      break;
+    case AnswerConstants.ANSWERS_RECEIVED:
+        resetAnswers(payload.answers);
+        QuestionStore.__emitChange();
+      break;
+    case AnswerConstants.ANSWER_RECEIVED:
+        addAnswer(payload.answer);
+        QuestionStore.__emitChange();
+      break;
+      // What in the world could possible be calling this method??
+    case QuestionConstants.MORE_QUESTIONS_RECEIVED:
+        resetQuestions(payload.questions);
+        QuestionStore.__emitChange();
+      break;
+  }
+};
 
-  dispatcherID: AppDispatcher.register(function (payload) {
-    switch(payload.actionType) {
-      case QuestionConstants.QUESTIONS_RECEIVED:
-          resetQuestions(payload.questions);
-        break;
-      case QuestionConstants.QUESTION_RECEIVED:
-          resetQuestion(payload.question);
-        break;
-      case AnswerConstants.ANSWERS_RECEIVED:
-          resetAnswers(payload.answers);
-        break;
-      case AnswerConstants.ANSWER_RECEIVED:
-          addAnswer(payload.answer);
-        break;
-      case QuestionConstants.MORE_QUESTIONS_RECEIVED:
-          resetQuestions(payload.questions);
-        break;
-    }
-  }),
-
-  addQuestionsIndexChangeListener: function (callback) {
-    this.on(QUESTIONS_INDEX_CHANGE_EVENT, callback);
-  },
-  removeQuestionsIndexChangeListener: function (callback) {
-    this.removeListener(QUESTIONS_INDEX_CHANGE_EVENT, callback);
-  },
-  changed: function () {
-    this.emit(QUESTIONS_INDEX_CHANGE_EVENT);
-  },
-
-  addAnswersIndexChangeListener: function (callback) {
-    this.on(ANSWERS_INDEX_CHANGE_EVENT, callback);
-  },
-  removeAnswersIndexChangeListener: function (callback) {
-    this.removeListener(ANSWERS_INDEX_CHANGE_EVENT, callback);
-  },
-  answersChanged: function () {
-    this.emit(ANSWERS_INDEX_CHANGE_EVENT);
-  },
-});
+addQuestionsIndexChangeListener: function (callback) {
+  this.on(QUESTIONS_INDEX_CHANGE_EVENT, callback);
+},
+removeQuestionsIndexChangeListener: function (callback) {
+  this.removeListener(QUESTIONS_INDEX_CHANGE_EVENT, callback);
+},
+changed: function () {
+  this.emit(QUESTIONS_INDEX_CHANGE_EVENT);
+},
+addAnswersIndexChangeListener: function (callback) {
+  this.on(ANSWERS_INDEX_CHANGE_EVENT, callback);
+},
+QuestionStore.removeAnswersIndexChangeListener = function (callback) {
+  this.removeListener(ANSWERS_INDEX_CHANGE_EVENT, callback);
+};
+QuestionStore.answersChanged = function () {
+  this.emit(ANSWERS_INDEX_CHANGE_EVENT);
+};
