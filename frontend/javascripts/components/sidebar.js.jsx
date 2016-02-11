4482b4f687bae9487bf16e5b6eec332aede03a2b
Harry <m.harry.miller@gmail.com>
2016-02-11 13:53:18 -0500

    everything is rendering correctly; about to work on sidebar

--- frontend/javascripts/components/sidebar.js.jsx
+++ frontend/javascripts/components/sidebar.js.jsx
@@ -1,14 +1,39 @@
-var React = require('react');
+var React = require('react'),
+    CheckboxGroup = require('react-checkbox-group');
 
 var SideBar = React.createClass({
+  getInitialState: function () {
+    return { value: ['General', 'Ruby', 'Philosophy', 'Javascript', 'React.js']};
+  },
+
+  handleChange: function () {
+    var selected = this.refs.topicsGroup.getCheckedValues();
+    console.log(selected);
+  },
+
   render: function () {
     return(
       <div className="page-left">
 		    <h2 className="title"> FEEDS </h2>
 		    <section className="page-left-content">
 		      Select feeds to display.
-          Make checkboxes here, etc, this component will be worth something
-		    </section>
+
+          <br/>
+
+          <CheckboxGroup name="topics" value={ this.state.value } ref="topicsGroup" onChange={ this.handleChange }>
+            <input type="checkbox" value="General" /> General
+            <br/>
+            <input type="checkbox" value="Ruby" /> Ruby
+            <br/>
+            <input type="checkbox" value="Philosophy" /> Philosophy
+            <br/>
+            <input type="checkbox" value="Javascript" /> Javascript
+            <br/>
+            <input type="checkbox" value="React.js "/> React.js
+            <br/>
+          </CheckboxGroup>
+
+          </section>
       </div>
     );
   }
